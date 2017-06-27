3bc341e4f03dc430f1924157ebb99f7ce95b3e67
Student21 <student21@gfrmedia.com>
2017-06-27 18:00:16 -0400

    lili cambio todo ot5ra ves

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+
 var users = require('./routes/users');
 var home = require('./routes/home');
 var menu = require('./routes/menu');
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+
 app.use('/users', users);
 app.use('/home', home);
 app.use('/menu',menu);
